e30f97902b6c7d00724dafcdb552e5d5f7e718b6
Josh <Joshsle22@gmail.com>
2025-01-22 15:24:20 -0400

    finished logic of game, now it works, finally. can be upgraded to add difficulty and actual logic behind the choices of the computer

--- tictactoe.js
+++ tictactoe.js
@@ -6,13 +6,18 @@ if(typeof document !== 'undefined'){
 let generator   =   document.getElementById('generate');
 let squareZone  =   document.getElementById('squareZone');
 let reload      =   0;
+let array = []
+let pC  = []
 
 let number  = function(n){
         
   return Math.ceil(Math.random()*n)
   }
     
-
+  for (let k = 0; k < 90; k++) {
+    array.push(number(10));
+    
+  }
 
 generator.addEventListener('click',function(){
     reload++
@@ -22,17 +27,29 @@ for (let square = 0; square < 9; square++) {
     let div     =   document.createElement('div');
     let player        =   document.createElement('p');
     let computer    =   document.createElement('p');
-
-
+    let valid
     
-
-    computer.id = 'cCheck'+i
-    div.id= 'unchecked' + i ;
-
+  
+    
     
 
 
+    pC  = array.filter((element, index) => array.indexOf(element) === index)
+    computer.id = 'cCheck';
+    div.id= 'unchecked'+pC[i];
+
+
+   
     generator.innerText =   'Play again';
+
+
+    computer.innerText  = 'O'
+    computer.style.fontSize = '3em';
+    computer.style.color  = 'blue';
+    computer.style.width  = 'fit-content';
+    computer.style.height  = 'fit-content';
+    computer.style.alignSelf  = 'center' ;
+    computer.style.placeSelf='center'; 
 
    
    
@@ -43,139 +60,90 @@ for (let square = 0; square < 9; square++) {
     div.style.alignItems  = 'center'
     div.style.justifyContent  = 'center'
     div.classList.add('square'+i.toString());
-    div.appendChild(computer)
     squareZone.appendChild(div);
+   
     
-
-
-  
-
     div.addEventListener('click',function(){
-      
-      switch(number(9)){
-        case  1:{ 
-          
-          document.getElementById('cCheck1').innerText  = 'O'
-          document.getElementById('cCheck1').style.fontSize = '3em';
-          document.getElementById('cCheck1').style.color  = 'blue';
-          document.getElementById('cCheck1').style.width  = 'fit-content';
-          document.getElementById('cCheck1').style.height  = 'fit-content';
-          document.getElementById('cCheck1').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck1').style.placeSelf='center'; 
-          console.log(1)
-          
-          break
-        }
-        case  2:{ 
-          document.getElementById('cCheck2').innerText  = 'O'
-          document.getElementById('cCheck2').style.fontSize = '3em';
-          document.getElementById('cCheck2').style.color  = 'blue';
-          document.getElementById('cCheck2').style.width  = 'fit-content';
-          document.getElementById('cCheck2').style.height  = 'fit-content';
-          document.getElementById('cCheck2').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck2').style.placeSelf='center'; 
-          console.log(2)
-          
-          break
-        }
-        case  3:{ 
-          document.getElementById('cCheck3').innerText  = 'O'
-          document.getElementById('cCheck3').style.fontSize = '3em';
-          document.getElementById('cCheck3').style.color  = 'blue';
-          document.getElementById('cCheck3').style.width  = 'fit-content';
-          document.getElementById('cCheck3').style.height  = 'fit-content';
-          document.getElementById('cCheck3').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck3').style.placeSelf='center'; 
-          console.log(3)
-          
-          break
-        }
-        case  4:{ 
-          document.getElementById('cCheck4').innerText  = 'O'
-          document.getElementById('cCheck4').style.fontSize = '3em';
-          document.getElementById('cCheck4').style.color  = 'blue';
-          document.getElementById('cCheck4').style.width  = 'fit-content';
-          document.getElementById('cCheck4').style.height  = 'fit-content';
-          document.getElementById('cCheck4').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck4').style.placeSelf='center'; 
-          console.log(4)
-          
-          break
-        }
-        case  5:{ 
-          document.getElementById('cCheck5').innerText  = 'O'
-          document.getElementById('cCheck5').style.fontSize = '3em';
-          document.getElementById('cCheck5').style.color  = 'blue';
-          document.getElementById('cCheck5').style.width  = 'fit-content';
-          document.getElementById('cCheck5').style.height  = 'fit-content';
-          document.getElementById('cCheck5').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck5').style.placeSelf='center'; 
-          console.log(5)
-          
-          break
-        }
-        case  6:{ 
-          document.getElementById('cCheck6').innerText  = 'O'
-          document.getElementById('cCheck6').style.fontSize = '3em';
-          document.getElementById('cCheck6').style.color  = 'blue';
-          document.getElementById('cCheck6').style.width  = 'fit-content';
-          document.getElementById('cCheck6').style.height  = 'fit-content';
-          document.getElementById('cCheck6').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck6').style.placeSelf='center'; 
-          console.log(6)
-          
-          break
-        }
-        case  7:{ 
-          document.getElementById('cCheck7').innerText  = 'O'
-          document.getElementById('cCheck7').style.fontSize = '3em';
-          document.getElementById('cCheck7').style.color  = 'blue';
-          document.getElementById('cCheck7').style.width  = 'fit-content';
-          document.getElementById('cCheck7').style.height  = 'fit-content';
-          document.getElementById('cCheck7').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck7').style.placeSelf='center'; 
-          console.log(7)
 
-          break
-        }
-        case  8:{ 
-          document.getElementById('cCheck8').innerText  = 'O'
-          document.getElementById('cCheck8').style.fontSize = '3em';
-          document.getElementById('cCheck8').style.color  = 'blue';
-          document.getElementById('cCheck8').style.width  = 'fit-content';
-          document.getElementById('cCheck8').style.height  = 'fit-content';
-          document.getElementById('cCheck8').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck8').style.placeSelf='center'; 
-          console.log(8)
-          
-          break
-        }
-        case  9:{ 
-          document.getElementById('cCheck9').innerText  = 'O'
-          document.getElementById('cCheck9').style.fontSize = '3em';
-          document.getElementById('cCheck9').style.color  = 'blue';
-          document.getElementById('cCheck9').style.width  = 'fit-content';
-          document.getElementById('cCheck9').style.height  = 'fit-content';
-          document.getElementById('cCheck9').style.alignSelf  = 'center' ;
-          document.getElementById('cCheck9').style.placeSelf='center'; 
-          console.log(9)
-          
-          break
-        }
+setTimeout(function(){
+
+  switch(valid){
+    case 0:{
+      if(document.getElementById('unchecked1') !== null){
+        document.getElementById('unchecked1').appendChild(computer)
+        document.getElementById('unchecked1').id='check'
+        
+      }else if(document.getElementById('unchecked1')=== null && document.getElementById('unchecked2') !== null){
+        document.getElementById('unchecked2').appendChild(computer)
+        document.getElementById('unchecked2').id='check'
+    
+      }else if(document.getElementById('unchecked2')=== null && document.getElementById('unchecked3') !==null){
+        document.getElementById('unchecked3').appendChild(computer)
+        document.getElementById('unchecked3').id='check'
+    
+      }else if(document.getElementById('unchecked3')=== null && document.getElementById('unchecked4') !==null){
+        document.getElementById('unchecked4').appendChild(computer)
+        document.getElementById('unchecked4').id='check'
+    
+      }else if(document.getElementById('unchecked4')=== null && document.getElementById('unchecked5') !==null){
+        document.getElementById('unchecked5').appendChild(computer)
+        document.getElementById('unchecked5').id='check'
+    
+      }else if(document.getElementById('unchecked5')=== null && document.getElementById('unchecked6') !==null){
+        document.getElementById('unchecked6').appendChild(computer)
+        document.getElementById('unchecked6').id='check'
+    
+      }else if(document.getElementById('unchecked6')=== null && document.getElementById('unchecked7') !==null){
+        document.getElementById('unchecked7').appendChild(computer)
+        document.getElementById('unchecked7').id='check'
+    
+      }else if(document.getElementById('unchecked7')=== null && document.getElementById('unchecked8') !==null){
+        document.getElementById('unchecked8').appendChild(computer)
+        document.getElementById('unchecked8').id='check'
+    
+      }else if(document.getElementById('unchecked8')=== null && document.getElementById('unchecked9') !==null){
+        document.getElementById('unchecked9').appendChild(computer)
+        document.getElementById('unchecked9').id='check'
+    
+      }else if(document.getElementById('unchecked9')=== null && document.getElementById('unchecked10') !==null){
+        document.getElementById('unchecked10').appendChild(computer)
+        document.getElementById('unchecked10').id='check'
+    
       }
+      break
+    }
+    case 1:
+      {
+        break
+      }
+  }
+  
+ 
 
-      
+},100)
+     
+     
+
+
+
   })
 
 
 
     div.addEventListener('click',function(){
 
-          
-        div.id= 'check'+i;
-        document.getElementById('check'+i).removeChild(computer)
+               if(div.id === 'check'){
+            console.log('hola')
+            valid = 1
+      }   else if(div.id!==  'check'){
+            div.id = 'check'+i;
+            div.appendChild(player);
+            valid = 0
+           
+      }
+        
+        
       
-  
         player.innerText  = 'X';
         player.style.fontSize = '3em';
         player.style.color  = 'red';
@@ -183,12 +151,12 @@ for (let square = 0; square < 9; square++) {
         player.style.height  = 'fit-content';
         player.style.alignSelf  = 'center'; 
         player.style.placeSelf='center';
-        div.appendChild(player);
-
+        
     })
 
    
 }
+
 
   if(reload===  2){
     location.reload()
